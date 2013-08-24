f7902bc59aa678457c98db93513f5208d3a6d70d
tam.s89 <tam.s89@gmail.com>
2013-08-24 21:38:47 +0100

    ++

--- ProjectEuler/Euler10.cs
+++ ProjectEuler/Euler10.cs
@@ -1,12 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Data.Common;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProjectEuler
 {
@@ -16,30 +11,22 @@ namespace ProjectEuler
     public class Euler10
     {
         // Find the sum of all the primes below two million.
-        public static void SummationOfPrimesBelowTwoMillion()
+        public static long SummationOfPrimesBelowTwoMillion()
         {
-            int sumOfPrimes = 0;
-            var primes = Sieve2((int)2e6);
-            foreach (var prime in primes)
-            {
-                sumOfPrimes += prime;
-            }
-
-            Console.WriteLine("Sum of primes below two million = {0}", sumOfPrimes);
-            Console.ReadLine();
+            var primes = Primes((int) 2e6);
+            return primes.Aggregate((l, l1) => l + l1);
         }
 
-
-        private static IEnumerable<int> Primes(int maximumPrime)
+        private static IEnumerable<long> Primes(int maximumPrime)
         {
             // HashSet pre-populated with initial values. Also HashSet inherently only allows unique values and all primes are unique.
-            var primes = new HashSet<int> { 2, 3, 5, 7 };
+            var primes = new HashSet<long> { 2, 3, 5, 7 };
 
             // Exclusion bit array, all values false by default.
             var exclude = new BitArray(maximumPrime + 1);
 
             // For the testing of the trial division 
-            var sqrtOfMax = Math.Ceiling(Math.Sqrt(maximumPrime));
+            var sqrtOfMax = Math.Sqrt(maximumPrime);
 
             // Increment in steps of 2 starting from 3 as all even numbers are multiples of 2 and are not primes; so these are 
             // avoided by incrementing 3 (odd) by an even number 2 which is always odd.
@@ -47,7 +34,7 @@ namespace ProjectEuler
             {
                 // If in exclusion list goto next value. 
                 if (exclude[i]) continue;
-                
+
                 if (i <= sqrtOfMax)
                     // A prime squared is not a prime
                     // Exclude all multiples of this prime; 2 * primeSquared which are even and so cannot be prime except for 2.
--- ProjectEuler/Program.cs
+++ ProjectEuler/Program.cs
@@ -1,16 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace ProjectEuler
 {
     public class Program
     {
+        private static Stopwatch sw = new Stopwatch();
+
         public static void Main(string[] args)
         {
-           Euler10.SummationOfPrimesBelowTwoMillion();
+            var res = Euler11.GreatestFourAdjacentNumbers();
+            Console.WriteLine(res);
+        }
+
+        private static void TotalTime(long timeTaken, string problem, dynamic result)
+        {
+            Console.WriteLine("Time elapsed: {0}. Problem: {1}. Result: {2}", timeTaken, problem, result.ToString());
         }
     }
 }
