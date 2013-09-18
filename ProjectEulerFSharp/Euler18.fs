38e3077da5049579bd6e4b98b8e8fd716eac0f4d
tam.s89 <tam.s89@gmail.com>
2013-09-18 22:25:11 +0100

    Euler18++

--- ProjectEulerFSharp/Euler18.fs
+++ ProjectEulerFSharp/Euler18.fs
@@ -20,29 +20,29 @@ open System.Linq
 
 module Euler18 =
 
-//    let triangle =
-//        [| [| 75; |];
-//           [| 95; 64; |];
-//           [| 17; 47; 82; |];
-//           [| 18; 35; 87; 10; |];
-//           [| 20; 04; 82; 47; 65; |];
-//           [| 19; 01; 23; 75; 03; 34; |];
-//           [| 88; 02; 77; 73; 07; 63; 67; |];
-//           [| 99; 65; 04; 28; 06; 16; 70; 92; |];
-//           [| 41; 41; 26; 56; 83; 40; 80; 70; 33; |];
-//           [| 41; 48; 72; 33; 47; 32; 37; 16; 94; 29; |];
-//           [| 53; 71; 44; 65; 25; 43; 91; 52; 97; 51; 14; |];
-//           [| 70; 11; 33; 28; 77; 73; 17; 78; 39; 68; 17; 57; |];
-//           [| 91; 71; 52; 38; 17; 14; 91; 43; 58; 50; 27; 29; 48; |];
-//           [| 63; 66; 04; 68; 89; 53; 67; 30; 73; 16; 69; 87; 40; 31; |];
-//           [| 04; 62; 98; 27; 23; 09; 70; 98; 73; 93; 38; 53; 60; 04; 23; |] |]
-
-    // Down & diagonally right so adjacent elements going down.
     let triangle =
-         [|[| 3; |];
-           [| 7; 4; |];
-           [| 2; 4; 6; |];
-           [| 8; 5; 9; 3; |]|];
+        [| [| 75; |];
+           [| 95; 64; |];
+           [| 17; 47; 82; |];
+           [| 18; 35; 87; 10; |];
+           [| 20; 04; 82; 47; 65; |];
+           [| 19; 01; 23; 75; 03; 34; |];
+           [| 88; 02; 77; 73; 07; 63; 67; |];
+           [| 99; 65; 04; 28; 06; 16; 70; 92; |];
+           [| 41; 41; 26; 56; 83; 40; 80; 70; 33; |];
+           [| 41; 48; 72; 33; 47; 32; 37; 16; 94; 29; |];
+           [| 53; 71; 44; 65; 25; 43; 91; 52; 97; 51; 14; |];
+           [| 70; 11; 33; 28; 77; 73; 17; 78; 39; 68; 17; 57; |];
+           [| 91; 71; 52; 38; 17; 14; 91; 43; 58; 50; 27; 29; 48; |];
+           [| 63; 66; 04; 68; 89; 53; 67; 30; 73; 16; 69; 87; 40; 31; |];
+           [| 04; 62; 98; 27; 23; 09; 70; 98; 73; 93; 38; 53; 60; 04; 23; |] |]
+
+//    // Down & diagonally right so adjacent elements going down.
+//    let triangle =
+//         [|[| 3; |];
+//           [| 7; 4; |];
+//           [| 2; 4; 6; |];
+//           [| 8; 5; 9; 3; |]|];
 
     let pathFinder =
         
@@ -70,6 +70,7 @@ module Euler18 =
            // 2. the value diagonally to the left and up.
            
            let mutable valueAbove = 0
+           let mutable sumWithAbove = 0
            try 
             valueAbove <- triangle.[i - 1].[x]
            with 
@@ -90,8 +91,10 @@ module Euler18 =
            sumList.Add(sumWithAbove)
 
          // Select max x element from sumList where x is the number of elements in the row above.
-         let filterValue = sumList.OrderByDescending(fun i -> i).ToList().[rowAbove.Length]
-         let filterForMaxX = sumList.Where(fun x -> x > filterValue).ToList()
-         summedRow.Add(filterForMaxX)
+         let list = new List<int>(sumList)
+         let filtervalues = sumList.OrderByDescending(fun i -> i).Take(rowAbove.Length).ToList()
+         let final = list.Where(fun x -> filtervalues.Contains(x)).ToList()
+            
+         summedRow.Add(filtervalues)
         
         summedRow.Last().Max() // max value from last array
